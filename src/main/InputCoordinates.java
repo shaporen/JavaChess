66d89002e2bf181e8f5e3fc50335dc385336cbfd
shaporen <shaporen@yandex.ru>
2023-12-22 05:14:24 +0300

    1.35

--- src/main/InputCoordinates.java
+++ src/main/InputCoordinates.java
@@ -4,6 +4,45 @@ import java.util.*;
 
 public class InputCoordinates {
 
-    Scanner scanner = new Scanner(System.in);
+    private static final Scanner scanner = new Scanner(System.in);
 
+    public static Coordinates input() {
+        while (true) {
+            System.out.println("Please enter coordinates (ex. a1)");
+            String line = scanner.nextLine();
+            if (line.length() != 2) {
+                System.out.println("Invalide format");
+                continue;
+            }
+
+            char fileChar = line.charAt(0);
+            char rankChar = line.charAt(1);
+
+            if (!Character.isLetter(fileChar)) {
+                System.out.println("Invalide format");
+            }
+
+            if (!Character.isDigit(rankChar)) {
+                System.out.println("Invalide format");
+            }
+
+            int rank = Character.getNumericValue(rankChar);
+            if ((rank < 1) || (rank > 8)) {
+                System.out.println("Invalide format");
+            }
+
+            File file = File.fromChar(fileChar);
+            if (file == null) {
+                System.out.println("Invalide format");
+                continue;
+            }
+
+            return new Coordinates(file, rank);
+        }
+    }
+
+    public static void main(String[] args) {
+        Coordinates coordinates = input();
+        System.out.println(coordinates);
+    }
 }
